b521c27bca5c049d7f59935ffd3e977764681ba5
Wan Youwen <1756429307@qq.com>
2018-06-12 22:41:55 +0800

    半直接视觉里程计 测试

--- svo/test/test_pipeline.cpp
+++ svo/test/test_pipeline.cpp
@@ -1,18 +1,5 @@
 // This file is part of SVO - Semi-direct Visual Odometry.
-//
-// Copyright (C) 2014 Christian Forster <forster at ifi dot uzh dot ch>
-// (Robotics and Perception Group, University of Zurich, Switzerland).
-//
-// SVO is free software: you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or any later version.
-//
-// SVO is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 半直接视觉里程计svo 测试　　
 
 #include <svo/config.h>
 #include <svo/frame_handler_mono.h>
@@ -26,71 +13,74 @@
 #include <vikit/atan_camera.h>
 #include <vikit/pinhole_camera.h>
 #include <opencv2/opencv.hpp>
-#include <sophus/se3.h>
+#include <sophus/se3.h>// 姿态李代数
 #include <iostream>
 #include "test_utils.h"
-
+//　命名空间
 namespace svo {
-
-class BenchmarkNode
-{
-  vk::AbstractCamera* cam_;
-  svo::FrameHandlerMono* vo_;
-
-public:
-  BenchmarkNode();
-  ~BenchmarkNode();
-  void runFromFolder();
-};
-
-BenchmarkNode::BenchmarkNode()
-{
-  cam_ = new vk::PinholeCamera(752, 480, 315.5, 315.5, 376.0, 240.0);
-  vo_ = new svo::FrameHandlerMono(cam_);
-  vo_->start();
-}
-
-BenchmarkNode::~BenchmarkNode()
-{
-  delete vo_;
-  delete cam_;
-}
-
-void BenchmarkNode::runFromFolder()
-{
-  for(int img_id = 2; img_id < 188; ++img_id)
-  {
-    // load image
-    std::stringstream ss;
-    ss << svo::test_utils::getDatasetDir() << "/sin2_tex2_h1_v8_d/img/frame_"
-       << std::setw( 6 ) << std::setfill( '0' ) << img_id << "_0.png";
-    if(img_id == 2)
-      std::cout << "reading image " << ss.str() << std::endl;
-    cv::Mat img(cv::imread(ss.str().c_str(), 0));
-    assert(!img.empty());
-
-    // process frame
-    vo_->addImage(img, 0.01*img_id);
-
-    // display tracking quality
-    if(vo_->lastFrame() != NULL)
+    // BenchmarkNode类
+    class BenchmarkNode
     {
-    	std::cout << "Frame-Id: " << vo_->lastFrame()->id_ << " \t"
-                  << "#Features: " << vo_->lastNumObservations() << " \t"
-                  << "Proc. Time: " << vo_->lastProcessingTime()*1000 << "ms \n";
+      vk::AbstractCamera* cam_;//针孔相机模型
+      svo::FrameHandlerMono* vo_;//单目视觉里程计 跟踪
 
-    	// access the pose of the camera via vo_->lastFrame()->T_f_w_.
+    public:
+      BenchmarkNode();
+      ~BenchmarkNode();
+      void runFromFolder();//运行
+    };
+    // 类构造函数
+    BenchmarkNode::BenchmarkNode()
+    {
+      cam_ = new vk::PinholeCamera(752, 480, 315.5, 315.5, 376.0, 240.0);
+      vo_ = new svo::FrameHandlerMono(cam_);
+      vo_->start();//启动视觉里程计 进行跟踪
     }
-  }
-}
+    // 类析构函数
+    BenchmarkNode::~BenchmarkNode()
+    {
+      delete vo_;
+      delete cam_;
+    }
+  
+// 运行借口
+    void BenchmarkNode::runFromFolder()
+    {
+      // 从第二帧开始跟踪
+      for(int img_id = 2; img_id < 188; ++img_id)
+      {
+        // 载入图像
+        std::stringstream ss;//图片文件地址字符串流
+        ss << svo::test_utils::getDatasetDir() << "/sin2_tex2_h1_v8_d/img/frame_"
+           << std::setw( 6 ) << std::setfill( '0' ) << img_id << "_0.png";//格式
+        if(img_id == 2)
+          std::cout << "reading image " << ss.str() << std::endl;
+        // opencv读取图像
+        cv::Mat img(cv::imread(ss.str().c_str(), 0));
+        assert(!img.empty());//　图像为空时退出
 
+        // 添加图像并处理　process frame
+        vo_->addImage(img, 0.01*img_id);
+
+        // 显示跟踪质量　display tracking quality
+        if(vo_->lastFrame() != NULL)//上一帧
+        {
+          std::cout << "Frame-Id: " << vo_->lastFrame()->id_ << " \t"
+                      << "#Features: " << vo_->lastNumObservations() << " \t"// 观测到的路标点数量
+                      << "Proc. Time: " << vo_->lastProcessingTime()*1000 << "ms \n";
+
+          // access the pose of the camera via vo_->lastFrame()->T_f_w_.
+        }
+      }
+    }
+  
 } // namespace svo
 
 int main(int argc, char** argv)
 {
   {
-    svo::BenchmarkNode benchmark;
-    benchmark.runFromFolder();
+    svo::BenchmarkNode benchmark;// 定义一个BenchmarkNode类的对象
+    benchmark.runFromFolder();//运行　跟踪
   }
   printf("BenchmarkNode finished.\n");
   return 0;
